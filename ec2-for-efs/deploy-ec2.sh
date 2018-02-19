39fdf69eba74caf9310b76007e0066b412f9b7cd
Michael Edge <edgema@c4b301d07c5d.ant.amazon.com>
2018-02-19 13:03:41 +0800

    new

---
+++ ec2-for-efs/deploy-ec2.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+AWSAccount=295744685835
+AWSAccountProfile=blog-tools
+region=us-east-2
+
+aws cloudformation deploy --stack-name ec2-cmd-client --template-file ec2-for-efs/ec2-for-efs.yaml \
+--capabilities CAPABILITY_NAMED_IAM \
+--parameter-overrides VPCId=vpc-21fb5749 CidrBlock=172.20.0.0/24 RouteTableID=rtb-f7ec759f KeyName=dlt-blockchain-key VolumeName=dltefs MountPoint=efsmount \
+--profile $AWSAccountProfile --region $region
+
+
+#aws cloudformation deploy --stack-name ec2-cmd-client-full --template-file ec2-for-efs/full-ec2-for-efs.yaml \
+#--capabilities CAPABILITY_NAMED_IAM \
+#--parameter-overrides KeyName=dlt-blockchain-key \
+#--profile $AWSAccountProfile --region $region
